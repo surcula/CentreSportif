3bc765894ea304040775993b6ef0e5c2fcd626c3
Franz <franz.anthoine@promsocatc.net>
2025-10-26 21:15:58 +0100

    Step Final : Ajout essai menu déroulant select.

--- src/main/java/interfaces/CitiesService.java
+++ src/main/java/interfaces/CitiesService.java
@@ -8,8 +8,14 @@ import java.util.List;
 public interface CitiesService {
     /**
      * Retrieves Active Cities
-     * @return
+     * @return all the active cities
      */
     Result<List<City>> getAllActiveCities();
+
+    /**
+     * Retrieves Cities by zipcode
+     * @param zip zipcode of the cities
+     * @return all the zipcode of the cities
+     */
     Result<List<City>> getActiveByZip(int zip);
 }
